a754fc1d5b135687e9fc1acce00d7c0b67b73563
jkomyno <skiabo97@gmail.com>
2020-12-10 01:18:20 +0100

    Added Docker image clustering/preprocess

---
+++ scripts/preprocess.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run \
+  -v "$(pwd)/data":/data \
+  -v "$(pwd)/python/preprocess":/usr/app/preprocess \
+  clustering/preprocess \
+  /data/original/input.fasta \
+  /data/preprocess/preprocessed.fasta \
+  1000
